ccf486b1e089823363e48459a567688f2b3de94c
ResendiK2 <gabriel.arantesresende@gmail.com>
2021-09-30 10:57:30 -0300

    finalização da primeira parte do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-import {ROUTES} from './app.routes'
+import { ROUTES } from './app.routes'
 
 // Components:
 import { AppComponent } from './app.component';
@@ -14,13 +14,15 @@ import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import {MenuComponent} from './restaurant-detail/menu/menu.component';
-import {MenuItemComponent} from './restaurant-detail/menu-item/menu-item.component';
-import {ShoppingCartComponent} from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 // Services:
-import {RestaurantsService} from './restaurants/restaurants.service';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+
 
 @NgModule({
   declarations: [
@@ -44,7 +46,9 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
-    RestaurantsService
+    RestaurantsService,
+    ShoppingCartService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [AppComponent]
 })
